f9d4fc79e4bbc1b0cd83c7a4b87d90143ce367a3
kuldeep <hytek112@gmail.com>
2022-10-31 19:27:38 +0530

    working with char and boolean datatypes

---
+++ javaFundamentals/DatatypesBool.java
@@ -0,0 +1,24 @@
+package javaFundamentals;
+
+public class DatatypesBool {
+    public static void main(String[] args) {
+        boolean b;
+        b = false;
+        System.out.println("b is " + b);
+        b = true;
+        System.out.println("b is " + b);
+        // bool using if - else
+        if(b){
+            System.out.println("I am b an i am "+b);
+        }else{
+            System.out.println("I am b an i am "+b);
+        }
+// some experimemnt
+        System.out.println("11 > 12 is " + (11>12));
+
+
+
+
+
+    }
+}
---
+++ javaFundamentals/DatatypesChar.java
@@ -0,0 +1,19 @@
+package javaFundamentals;
+
+public class DatatypesChar {
+    public static void main(String[] args) {
+              //demo char datatype
+              char ch1 , ch2;
+              ch1 = 88;//code for X
+              ch2 = 'Y';
+              System.out.print("ch1 and ch2: ");
+              System.out.println(ch1 + " " + ch2);
+      
+      // Another Char Program to demostrate the use of char datatype
+              // performing Arithmetic operation on char
+              ch1 = 'X';
+              System.out.println("ch1 contains " + ch1);
+              ch1++; //increment the value of char datatype
+              System.out.println("ch1 is now  " + ch1);
+    }
+}
