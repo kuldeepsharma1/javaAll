c104b686cf172a4e0336c4cd84bb7c329ecdf414
kuldeep <hytek112@gmail.com>
2022-10-31 16:47:29 +0530

    basic edit

--- Basic.java
+++ Basic.java
@@ -5,16 +5,17 @@ class Basic{
     public static void main(String args[]){
         System.out.println("Hello to HYTEK JAVA");
         System.out.println("Hello to HYTEK JAVA\n");
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter Your Age:>>");
-        int age = sc.nextInt();
-        
-        // check the user age and apply rules
-        if (age <= 18 ){
-            System.out.println(" you can't drive");
-        }
-        else{
-            System.out.println(" you can drive");
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter Your Age:>>");
+            int age = sc.nextInt();
+            
+            // check the user age and apply rules
+            if (age <= 18 ){
+                System.out.println(" you can't drive");
+            }
+            else{
+                System.out.println(" you can drive");
+            }
         }
 
 
